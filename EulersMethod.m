1b6e0f148650bf1aa19854f7130dec6bd98a341f
Supta Richard Philip <supta.philip@gmail.com>
2019-03-01 17:45:16 +0600

    Taylor Series and Eulers Methods

---
+++ EulersMethod.m
@@ -0,0 +1,19 @@
+﻿#Eulers methods
+
+#dy/dx = y.^2-4*x, y(0)=1, h = 0.01
+
+
+x = zeros(201,1);
+y = zeros(201,1);
+
+x(1) = 0;
+y(1) = 1;
+h= 0.01;
+for i=1:200
+    x(i+1) = x(i)+h;
+    y(i+1) = y(i)+h.*((y(i).^2)-(4.*x(i)));
+end
+
+#plot(x,y)
+#print -dpng figure.png
+[x,y]
---
+++ TaylorSeries.m
@@ -0,0 +1,32 @@
+﻿clear all;
+clc;
+close all;
+
+x = -2:0.1:2;
+y = exp(x);
+
+fig = figure();
+
+set(fig,"color","white")
+plot(x,y,"Linewidth",2)
+grid on
+xlabel('x')
+ylabel('y')
+
+#change N for order of equations
+# Now is third order
+N =3;
+
+yest = 0*y
+
+for n=0:N
+    yest = yest*(x.^n)./factorial(n)
+end
+
+hold on
+plot(x,yest,"red","LineWidth",2)
+legend("Actual Function","Taylor series expansion")
+    
+
+
+
